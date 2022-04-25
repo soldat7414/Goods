78da174fa7ad43a8a9894eaedce4b291c218605c
soldat7414 <soldat7414@gmail.com>
2022-04-25 23:15:37 +0300

    Add properties

---
+++ src/repository/Properties.java
@@ -0,0 +1,14 @@
+package repository;
+
+/**
+ * @author Soldatenko Ihor
+ * @version 1.0.0
+ */
+
+public class Properties {
+
+    public final static String DB_DRIVER = "jdbc:sqlite:";
+    public final static String DB_BASE_URL = "F:\\programming\\Hillel school\\projects\\ownworks\\ownwork4\\goods\\database\\";
+    public final static String DB_NAME = "goods.db";
+    public final static String TABLE_NAME = "goods";
+}
