e12934d0c0b5249e2a2e61cc97d9d9f05856e3c2
soldat7414 <soldat7414@gmail.com>
2022-04-25 23:16:32 +0300

    Add entity

---
+++ src/entity/Product.java
@@ -0,0 +1,48 @@
+package entity;
+
+public class Product {
+
+    private String title;
+    private String manufacturer;
+    private String description;
+    private double price;
+
+    public Product(String title, String manufacturer, String description, double price) {
+        this.title = title;
+        this.manufacturer = manufacturer;
+        this.description = description;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
